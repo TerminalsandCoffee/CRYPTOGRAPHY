f0234d6661168e17f01c1a3b8e334ee20c08d6db
Rafael <rgmllc@yahoo.com>
2024-08-22 23:17:52 -0400

    Create CaesarCipher.py

---
+++ CaesarCipher.py
@@ -0,0 +1,28 @@
+# Function to decrypt the cipher text using Caesar Cipher
+def decrypt_caesar_cipher(ciphertext, shift):
+    decrypted_text = ''
+    for char in ciphertext:
+        if char.isalpha():
+            shifted = ord(char) - shift
+            if char.islower():
+                if shifted < ord('a'):
+                    shifted += 26
+            elif char.isupper():
+                if shifted < ord('A'):
+                    shifted += 26
+            decrypted_text += chr(shifted)
+        else:
+            decrypted_text += char
+    return decrypted_text
+
+# Given cipher text
+ciphertext = "fxxmjbfxltnslymwtzlmymjbttixxmjrjybnymfbtqkbmtmfifajwdlwjfyrnsiytjfymjwzugzymjifwjistygjhfzxjtkxtrjbttihzyyjwxbtwpnslsjfwgdnsymjktwjxymjfxpjimjwbmjwjxmjbfxltnslymjuttwhmnqibmtinistypstbymfynybfxifsljwtzxytxyfdfsiyfqpytfbtqkxfniytmnrnfrltnslytxjjrdlwfsirtymjwfsi hfwwdmjwfhfpjfsifqnyyqjutytkgzyyjwkwtrrdrtymjw"
+
+# Try different shifts to find the correct decryption
+possible_decryptions = {}
+for shift in range(1, 26):
+    decrypted_text = decrypt_caesar_cipher(ciphertext, shift)
+    possible_decryptions[shift] = decrypted_text
+
+# Display all possible decrypted texts
+possible_decryptions
